0adf313cfbf3fbfd4d7cace7625f15195d9680fb
Nick-Giampetro <113528133+Nick-Giampetro@users.noreply.github.com>
2023-04-17 10:30:12 -0400

    trying to figure out how feedback works

--- AERSP304_Proj3.m
+++ AERSP304_Proj3.m
@@ -10,7 +10,7 @@ A = [ -0.03 -32.1 0 0; 0 0 0 56.7; -0.00025 0 -0.313 56.7; 0 0 -0.0139 -0.426];
 B = [0; 0; 0.232; 0.0203];
 C = [0 1 0 0];
 D = 0;
-sys = ss(A,B,C,D);
+G_open = ss(A,B,C,D);
 
 % Convert State Space Representation to a Transfer Function
 [num,den] = ss2tf(A,B,C,D);          % num returns transfer function numerator coefficients, den returns denominator coefficients
@@ -20,7 +20,11 @@ sys = ss(A,B,C,D);
 
 % Step Input Response
 opt = stepDataOptions('StepAmplitude',.2);           % inputting step input of elevator
-step(sys,opt);                                       % response for the step input
+step(G_open,opt);                                       % response for the step input
 
-
+% Closed Transfer Function
+thetaDes = tf([0,0,0,0,0.2]);
+t = tf(num)
+Gcl = feedback(tf(num),thetaDes) ;
+step(Gcl,opt);
 
