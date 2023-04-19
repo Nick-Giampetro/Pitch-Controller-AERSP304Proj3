c222759fa7b33f27b8e6d3c043154385da24451a
Nick-Giampetro <113528133+Nick-Giampetro@users.noreply.github.com>
2023-04-19 10:51:57 -0400

    added rltool. idk what it does

--- AERSP304_Proj3.m
+++ AERSP304_Proj3.m
@@ -10,22 +10,28 @@ A = [ -0.03 -32.1 0 0; 0 0 0 56.7; -0.00025 0 -0.313 56.7; 0 0 -0.0139 -0.426] ;
 B = [0; 0; 0.232; 0.0203] ;
 C = [0 1 0 0] ;
 D = 0 ;
-G_open = ss(A,B,C,D) ;
+G = ss(A,B,C,D) ;
 
 % Convert State Space Representation to a Transfer Function
-[num,den] = ss2tf(A,B,C,D)           % num returns transfer function numerator coefficients, den returns denominator coefficients for open-loop transfer function
+[num,den] = ss2tf(A,B,C,D) ;        % num returns transfer function numerator coefficients, den returns denominator coefficients for open-loop transfer function
 
 % Find Poles and Zeros
 [O,X,k] = tf2zp(num,den) ;
 
 % Step Input Response
 opt = stepDataOptions('StepAmplitude',.2) ;           % inputting step input of elevator
-step(G_open,opt) ;                                       % response for the step input
+figure
+step(G,opt) ;                                       % response for the step input
 
 % Closed Transfer Function
-K = 1
-
-Gcl = feedback(G_open,K)
+K = 1 ;
+Gcl = feedback(G,K) ;
 
 figure
 step(Gcl,opt)
+
+
+
+%Root Locus
+
+rltool(Gcl,0.2)
